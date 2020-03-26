21a82afd9aac5078c5b9b0266f0df42718e7f36c
Steve Bauman <steven_bauman@outlook.com>
2020-03-26 11:38:28 -0400

    Rescue unserialization in case of exception

--- src/Jobs/GenerateObjectChanges.php
+++ src/Jobs/GenerateObjectChanges.php
@@ -75,11 +75,18 @@ class GenerateObjectChanges implements ShouldQueue
 
             $before = array_key_exists($attribute, $this->old) ? $this->old[$attribute] : [];
 
+            // Our values will be serialized from the DetectChanges pipe. We
+            // will attempt to unserialize the values, and if it values we
+            // will simply save the values that cannot be unserialized.
+            $after = rescue(function () use ($values) {
+                return unserialize($values);
+            }, $values);
+
             $change->fill([
                 'ldap_updated_at' => $this->when,
                 'attribute' => $attribute,
                 'before' => $before,
-                'after' => unserialize($values),
+                'after' => $after,
             ])->save();
         }
     }
