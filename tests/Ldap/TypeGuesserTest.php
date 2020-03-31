2c0150e8a09c401d40fdb23ef5ea5c0f9cd28f6f
Steve Bauman <steven_bauman@outlook.com>
2020-03-31 09:46:26 -0400

    Update TypeGuesserTest.php

--- tests/Ldap/TypeGuesserTest.php
+++ tests/Ldap/TypeGuesserTest.php
@@ -16,6 +16,18 @@ class TypeGuesserTest extends TestCase
         $this->assertEquals(TypeGuesser::TYPE_USER, (new TypeGuesser($classes))->get());
     }
 
+    public function test_computer_type_is_determined_from_other_valid_types()
+    {
+        // The computer class will always be last, as it inherits the user object class.
+        $this->assertEquals(TypeGuesser::TYPE_COMPUTER, (new TypeGuesser([
+            'user', 'computer',
+        ]))->get());
+
+        $this->assertEquals(TypeGuesser::TYPE_USER, (new TypeGuesser([
+            'computer', 'user',
+        ]))->get());
+    }
+
     public function test_null_is_returned_when_type_cannot_be_guessed()
     {
         $classes = ['foo', 'bar', 'baz'];
