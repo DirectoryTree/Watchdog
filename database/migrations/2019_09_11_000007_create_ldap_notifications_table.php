582db7361d6d7e35fabae750189077587f790499
Steve Bauman <steven_bauman@outlook.com>
2020-04-28 14:55:41 -0400

    Set a default value for 'sent'

--- database/migrations/2019_09_11_000007_create_ldap_notifications_table.php
+++ database/migrations/2019_09_11_000007_create_ldap_notifications_table.php
@@ -20,7 +20,7 @@ class CreateLdapNotificationsTable extends Migration
             $table->string('watchdog');
             $table->string('notification');
             $table->string('channels');
-            $table->boolean('sent');
+            $table->boolean('sent')->default(false);
             $table->longText('data')->nullable();
         });
     }
